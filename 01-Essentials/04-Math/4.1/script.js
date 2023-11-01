8cfd916da92f301a6c75543ef928ab26013dd976
Mohammed Al-Daragily <mohammed.aldaragily@gmail.com>
2023-11-01 15:11:18 +0200

    4.1

---
+++ 01-Essentials/04-Math/4.1/script.js
@@ -0,0 +1,9 @@
+var radius = parseInt(prompt('Enter a circle radius'));
+alert(`Area = ${Math.PI * Math.pow(radius, 2)}`);
+
+var number = parseInt(prompt('Enter a number'));
+alert(`SQRT = ${Math.sqrt(number)}`);
+
+var angle = parseInt(prompt('Enter an angle'));
+alert(`cos(${angle}) = ${(Math.cos(angle * Math.PI / 180)).toFixed(4)}`);
+
